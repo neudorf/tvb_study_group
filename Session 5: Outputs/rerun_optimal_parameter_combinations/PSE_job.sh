b1666bd1c1db543b3b79e3259205c57d282fee4f
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-04-22 02:58:59 -0600

    Update PSE_job.sh

--- Session 5: Outputs/rerun_optimal_parameter_combinations/PSE_job.sh
+++ Session 5: Outputs/rerun_optimal_parameter_combinations/PSE_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=YOUR_EMAIL_HERE(e.g. hello@gmail.com)
 #SBATCH --mail-type=FAIL
 #SBATCH --mem=8000MB
-#SBATCH --time=0-1:00
+#SBATCH --time=0-5:59
 
 # This script submits one or more simulations in a single job for PSE
 
@@ -11,7 +11,7 @@
 ### ====================================
 
 # Location of your submission script directory
-SUBMISSION_SCRIPT_DIR='/path/to/Session_4-Jobs_and_PSE'
+SUBMISSION_SCRIPT_DIR='/path/to/Session_5-Jobs_and_PSE'
 
 # Load in env
 module load scipy-stack
@@ -23,13 +23,14 @@ module load scipy-stack
 
 # Parameters from arguments
 param_file=$1
-subject=$2
-num_sims_per_job=$3
-time_per_sim=$4
-weights_file_pattern=$5
-results_file_pattern=$6
-FCD_file_pattern=$7
-empFUNC_dir=$8
+num_sims_per_job=$2
+time_per_sim=$3
+weights_file_pattern=$4
+results_file_pattern=$5
+FCD_file_pattern=$6
+empFUNC_dir=$7
+dt=$8 
+sim_len=$9
 
 # Calculate the starting and ending line based on the SLURM_ARRAY_TASK_ID
 start_line=$(((SLURM_ARRAY_TASK_ID - 1) * num_sims_per_job + 1))
@@ -39,6 +40,6 @@ end_line=$((start_line + num_sims_per_job - 1))
 
 
 # Read the parameters for the current job from start_line to end_line
-sed -n "${start_line},${end_line}p" $param_file | while IFS=' ' read -r noise G dt sim_len; do
+sed -n "${start_line},${end_line}p" $param_file | while IFS=' ' read -r subject noise G metric_value; do
   python ${SUBMISSION_SCRIPT_DIR}/single_sim_handler.py $subject $noise $G $dt $sim_len $time_per_sim $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $weights_file_pattern $results_file_pattern $FCD_file_pattern $empFUNC_dir
 done
