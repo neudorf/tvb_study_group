bb08471209ced2a2d9fc7b3fb1e351887cbf225c
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-04-22 01:18:30 -0600

    Create create_emp_funcfeat.py

---
+++ Session 5: Outputs/empirical_functional_features/create_emp_funcfeat.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import showcase1_ageing as utils
+
+### TO EDIT HERE
+## =============================
+
+save_dir="/path/to/empFUNC/saves"
+ts_file_pattern = '/path/to/{sub}/rfMRI_0.ica_time_series.txt'
+
+## =============================
+### TO EDIT HERE
+
+
+def read_time_series(file_path):
+    if os.path.exists(file_path):
+        # Assuming the file is space-separated
+        return pd.read_csv(file_path, sep=" ", header=None)
+    else:
+        print(f"File not found: {file_path}")
+        return None
+
+def compute_fc_matrix(ts):
+    ts_transposed = ts.T
+    rsFC = np.corrcoef(ts_transposed)
+    rsFC = rsFC - np.diag(np.diagonal(rsFC))
+
+    return rsFC
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <subject_id>")
+        sys.exit(1)
+
+    sub = sys.argv[1]
+    ts_file_path=ts_file_pattern.format(sub=sub)
+    ts = read_time_series(ts_file_path)
+    if ts is not None:
+        ts = ts.iloc[:144, :]  # Keep only the first 144 rows (timepoints)
+        ts = ts.to_numpy()
+        rsFC = compute_fc_matrix(ts)
+        
+        # Define path for saving the FC matrix
+        save_path = f'{save_dir}/{sub}_empFC.npy'
+        np.save(save_path, rsFC)
+
+        FCD, _ = utils.compute_fcd(ts, win_len=20)
+        save_path = f'{save_dir}/{sub}_empFCD.npy'
+        np.save(save_path, FCD)
+
+        FCD_VAR_OV_vect= np.var(np.triu(FCD, k=20))
+        save_path = f'{save_dir}/{sub}_empFCDvar.npy'
+        np.save(save_path, FCD_VAR_OV_vect)
