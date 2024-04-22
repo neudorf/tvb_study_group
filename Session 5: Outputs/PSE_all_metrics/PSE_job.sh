1996703d0c3860bc7f08b984a700e8428e64af61
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-04-22 02:03:04 -0600

    Update PSE_job.sh

--- Session 5: Outputs/PSE_all_metrics/PSE_job.sh
+++ Session 5: Outputs/PSE_all_metrics/PSE_job.sh
@@ -29,6 +29,7 @@ time_per_sim=$4
 weights_file_pattern=$5
 results_file_pattern=$6
 FCD_file_pattern=$7
+empFUNC_dir=$8
 
 # Calculate the starting and ending line based on the SLURM_ARRAY_TASK_ID
 start_line=$(((SLURM_ARRAY_TASK_ID - 1) * num_sims_per_job + 1))
@@ -39,5 +40,5 @@ end_line=$((start_line + num_sims_per_job - 1))
 
 # Read the parameters for the current job from start_line to end_line
 sed -n "${start_line},${end_line}p" $param_file | while IFS=' ' read -r noise G dt sim_len; do
-  python ${SUBMISSION_SCRIPT_DIR}/single_sim_handler.py $subject $noise $G $dt $sim_len $time_per_sim $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $weights_file_pattern $results_file_pattern $FCD_file_pattern
+  python ${SUBMISSION_SCRIPT_DIR}/single_sim_handler.py $subject $noise $G $dt $sim_len $time_per_sim $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $weights_file_pattern $results_file_pattern $FCD_file_pattern $empFUNC_dir
 done
