979967e30f007dd0c8b50d7b3408114c65e67e35
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-04-22 09:49:17 -0600

    Update optimal_sim_job.sh

--- Session 5: Outputs/rerun_optimal_parameter_combinations/optimal_sim_job.sh
+++ Session 5: Outputs/rerun_optimal_parameter_combinations/optimal_sim_job.sh
@@ -31,6 +31,7 @@ FCD_file_pattern=$6
 empFUNC_dir=$7
 dt=$8 
 sim_len=$9
+sim_file_pattern=$10
 
 # Calculate the starting and ending line based on the SLURM_ARRAY_TASK_ID
 start_line=$(((SLURM_ARRAY_TASK_ID - 1) * num_sims_per_job + 1))
@@ -41,5 +42,5 @@ end_line=$((start_line + num_sims_per_job - 1))
 
 # Read the parameters for the current job from start_line to end_line
 sed -n "${start_line},${end_line}p" $param_file | while IFS=' ' read -r subject noise G metric_value; do
-  python ${SUBMISSION_SCRIPT_DIR}/single_sim_handler.py $subject $noise $G $dt $sim_len $time_per_sim $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $weights_file_pattern $results_file_pattern $FCD_file_pattern $empFUNC_dir
+  python ${SUBMISSION_SCRIPT_DIR}/single_sim_handler.py $subject $noise $G $dt $sim_len $time_per_sim $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $weights_file_pattern $results_file_pattern $FCD_file_pattern $empFUNC_dir $sim_file_pattern
 done
